a5ee179cd6e7fcb2f577bbc80c58ac9f04d3f973
Selvamuthukumar <selvasmarter1715@gmail.com>
2023-11-15 20:02:15 +0530

    API call

--- src/app/user/output-page/output-page.page.ts
+++ src/app/user/output-page/output-page.page.ts
@@ -5,7 +5,7 @@ import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 import { EdenAIService } from 'src/app/services/eden-ai.service';
 import { Base64toURLService } from 'src/app/services/base64to-url.service';
-import { tap } from 'rxjs';
+import { tap } from 'rxjs/operators'; 
 
 @Component({
   selector: 'app-output-page',
@@ -14,8 +14,8 @@ import { tap } from 'rxjs';
 })
 export class OutputPagePage implements OnInit {
   base64Image: any;
-  caption = "This is an image of the cat looking curiously.";
-  sensitivityValue = "30%";
+  caption = "";
+  sensitivityValue = "";
 
   constructor(
     private storage: Storage,
@@ -23,42 +23,41 @@ export class OutputPagePage implements OnInit {
     private location: Location,
     private router: Router,
     private edenAIService: EdenAIService,
-    private base64toUrl : Base64toURLService
+    private base64toUrl: Base64toURLService
   ) {}
 
   async ngOnInit() {
     await this.storage.create();
-    this.base64Image = await this.storage.get("imageData");
-    this.Base64ToUrl()
+    this.base64Image = await this.storage.get('imageData');
+    this.Base64ToUrl();
   }
 
   Base64ToUrl() {
-    console.log("working 1");
-    
+    console.log('working 1');
     const base64Content = this.base64Image;
     const filename = 'Cos-eyes_AI.png';
-    console.log("working 2");
-  
-    this.base64toUrl.uploadBase64Image(base64Content, filename)
-      .subscribe(
-        (response) => {
+    console.log('working 2');
+    this.base64toUrl
+      .uploadBase64Image(base64Content, filename)
+      .pipe(
+        tap((response) => {
           console.log('Upload successful', response);
           this.getCaption();
-        },
-        (error) => {
-          console.error('Error uploading image', error);
-        }
-      );
+        }),
+      )
+      .subscribe(); 
   }
-  
 
   getCaption() {
-    this.edenAIService.performObjectDetection(this.base64Image).pipe(
-      tap((response) => {
-        console.log(response);
-        this.caption = response.caption;
-      })
-    ).subscribe();
+    this.edenAIService
+      .performObjectDetection(this.base64Image)
+      .pipe(
+        tap((response) => {
+          console.log(response);
+          this.caption = response.caption;
+        })
+      )
+      .subscribe(); 
   }
 
   back() {
@@ -67,9 +66,9 @@ export class OutputPagePage implements OnInit {
 
   playAudio() {
     this.textToSpeech
-      .speak(this.caption)
-      .then(() => console.log('Done'))
-      .catch((reason: any) => console.log(reason));
+    .speak(this.caption)
+    .then(() => console.log('Done'))
+    .catch((reason: any) => console.log(reason));
   }
 
   sensitivity() {
